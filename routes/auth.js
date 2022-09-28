57dff2c4df610169be63b7880fe354219ca0dea8
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-28 01:25:12 -0300

    Criptografando a senha do usuário

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const CryptoJS = require("crypto-js")
 
 //Registro
 router.post("/register", async (req, res) => {
     const newUser = new User({
-        username: req.body?.username,
-        email: req.body?.email,
-        password: req.body?.password,
+        username: req.body.username,
+        email: req.body.email,
+        //Criptografando a senha
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
 
     try {
