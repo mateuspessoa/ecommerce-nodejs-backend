69d5d99f323e05eea03a35b0ee8cbc99b31b5f0b
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-28 01:59:47 -0300

    Gerando Token de acesso com JWT para o usuário

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const User = require("../models/User")
 const CryptoJS = require("crypto-js")
+const jwt = require("jsonwebtoken")
 
 //Registro
 router.post("/register", async (req, res) => {
@@ -39,10 +40,19 @@ router.post("/login", async (req, res) => {
             return
         }
 
+        //Gerando um Token de acesso ao usuário
+        const accessToken = jwt.sign({
+                id: user._id,
+                isAdmin: user.isAdmin,
+            }, 
+            process.env.JWT_SEC,
+            {expiresIn: "3d"}
+        );
+
         //Excluindo a senha do retorno para não ser exibida
         const { password, ...others } = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({ ...others, accessToken });
 
     }catch(err) {
         res.status(500).json(err);
