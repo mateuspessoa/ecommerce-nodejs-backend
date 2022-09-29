0f2259eb571c36e361f704a7f0f76562f94931ae
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-29 03:02:18 -0300

    Pegar todos os usuários ou os 5 mais recentes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const CryptoJS = require("crypto-js")
 
 //Atualização dos dados do usuário
@@ -31,6 +31,38 @@ router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json("Usuário Deletado")
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Pegar um usuário
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id)
+
+        //Serve para não revelar a senha do usuário
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//Pegar todos os usuários
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new;
+
+    try {
+
+        //Essa query serve para limitar o resultado em 5 usuários, caso tena um "nwe=true" na URL
+        const users = query 
+            //Pegar apenas o usuário mais recente
+            ? await User.find().sort({_id: -1}).limit(5) 
+            //Pega todos os usuários
+            : await User.find();
+        res.status(200).json(users);
     }catch(err){
         res.status(500).json(err)
     }
