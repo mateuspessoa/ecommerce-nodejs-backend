b040aad242451a446e3e83bb4709c8688b94e48a
Mateus de Oliveira Pessoa <mateuspessoadev@gmail.com>
2022-09-30 13:26:35 -0300

    Todas as funções de CRUD para os Produtos

--- routes/product.js
+++ routes/product.js
@@ -16,95 +16,69 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
 
 });
 
-//Atualização dos dados do usuário
-/*router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
-
-    if(req.body.password) {
-        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString();
-    }
+//Atualização dos dados do produto
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 
     try {
 
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
 
             //Pegue tudo o que tá no body e atualize
             $set: req.body
         },{new: true});
 
-        res.status(200).json(updatedUser);
+        res.status(200).json(updatedProduct);
 
     }catch(err){
         res.status(500).json(err);
     }
 });
 
-//Deletar Usuário
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+//Deletar Produto
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
 
     try {
-        await User.findByIdAndDelete(req.params.id)
-        res.status(200).json("Usuário Deletado")
+        await Product.findByIdAndDelete(req.params.id)
+        res.status(200).json("Produto Deletado")
     }catch(err){
         res.status(500).json(err)
     }
 })
 
-//Pegar um usuário
-router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+//Pegar um produto
+router.get("/find/:id", async (req, res) => {
 
     try {
-        const user = await User.findById(req.params.id)
+        const product = await Product.findById(req.params.id)
 
-        //Serve para não revelar a senha do usuário
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        res.status(200).json(product);
     }catch(err){
         res.status(500).json(err)
     }
 })
 
-//Pegar todos os usuários
-router.get("/", verifyTokenAndAdmin, async (req, res) => {
-    const query = req.query.new;
+//Pegar todos os produtos
+router.get("/", async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
 
     try {
+        let products;
 
-        //Essa query serve para limitar o resultado em 5 usuários, caso tena um "nwe=true" na URL
-        const users = query 
-            //Pegar apenas o usuário mais recente
-            ? await User.find().sort({_id: -1}).limit(5) 
-            //Pega todos os usuários
-            : await User.find();
-        res.status(200).json(users);
+        if(qNew){
+            products = await Product.find().sort({createdAt: -1}).limit(1)
+        } else if(qCategory) {
+            products = await Product.find({categories: {
+                $in: [qCategory],
+            }});
+        }else {
+            products = await Product.find();
+        }
+
+        res.status(200).json(products);
     }catch(err){
         res.status(500).json(err)
     }
 })
-
-//Pegar as estatísticas do usuário
-router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-    const date = new Date();
-    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-  
-    try {
-      const data = await User.aggregate([
-        { $match: { createdAt: { $gte: lastYear } } },
-        {
-          $project: {
-            month: { $month: "$createdAt" },
-          },
-        },
-        {
-          $group: {
-            _id: "$month",
-            total: { $sum: 1 },
-          },
-        },
-      ]);
-      res.status(200).json(data)
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });*/
 
 module.exports = router;
